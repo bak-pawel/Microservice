851ba26e691a563e00920291849fb437367e4d9f
bak <pawelbak96@gmail.com>
2023-12-05 19:00:45 +0100

    clear tests

--- src/test/java/com/bak/paw/BookEndpointTest.java
+++ src/test/java/com/bak/paw/BookEndpointTest.java
@@ -16,62 +16,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@UnitTest
-@ExtendWith(MockitoExtension.class)
 public class BookEndpointTest {
-
-    @Mock
-    BookService bookService;
-
-    @InjectMocks
-    BookEndpoint unitUnderTest;
-
-    @Test
-    public void findAll() {
-        when(bookService.findAllFromDb()).thenReturn(getAllBooks());
-        assertThat(unitUnderTest.getAll()).hasSize(2);
-    }
-
-    @Test
-    public void findBookByName() {
-        when(bookService.findByName(any())).thenReturn(getOneBook());
-        assertThat(unitUnderTest.findByName("Dziady")).extracting(Book::getBookName).isEqualTo("Dziady");
-    }
-
-    @Test
-    public void addBook(){
-        when(bookService.addBook(any())).thenReturn(getOneBook());
-        assertThat(unitUnderTest.add(getOneBook())).extracting(Book::getBookName).isEqualTo("Dziady");
-    }
-
-    @Test
-    public void deleteBook(){
-        when(bookService.deleteBook(any())).thenReturn(getOneBook());
-        assertThat(unitUnderTest.delete(getOneBook())).extracting(Book::getBookName).isEqualTo("Dziady");
-    }
-
-    private List<Book> getAllBooks() {
-        return List.of(Book.builder()
-                        .id(UUID.randomUUID())
-                        .bookName("Dziady")
-                        .pageAmount("233")
-                        .author("Adam Mickiewicz")
-                        .build(),
-                Book.builder()
-                        .id(UUID.randomUUID())
-                        .bookName("Romeo i Julia")
-                        .pageAmount("123")
-                        .author("William Shakespeare")
-                        .build());
-
-    }
-    private Book getOneBook(){
-       return Book.builder()
-                .id(UUID.randomUUID())
-                .bookName("Dziady")
-                .pageAmount("233")
-                .author("Adam Mickiewicz")
-                .build();
-    }
 
 }
--- src/test/java/com/bak/paw/service/BookServiceTest.java
+++ src/test/java/com/bak/paw/service/BookServiceTest.java
@@ -17,46 +17,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@UnitTest
-@ExtendWith(MockitoExtension.class)
 class BookServiceTest {
 
-    @Mock
-    Logger log;
-
-    @Mock
-    BookRepository bookRepository;
-
-    @InjectMocks
-    BookService unitUnderTest;
-
-    @Test
-    public void findByName() {
-        when(bookRepository.findAny(any())).thenReturn(renderBook());
-        assertThat(unitUnderTest.findByName("Dziady")).extracting(Book::getBookName).isEqualTo("Dziady");
-    }
-    @Test
-    public void findAll() {
-        when(bookRepository.findAllFromDb()).thenReturn(List.of());
-        assertThat(unitUnderTest.findAllFromDb()).hasSize(0);
-    }
-    @Test
-    public void addBook() {
-        when(bookRepository.putDataToDB(any())).thenReturn(renderBook());
-        assertThat(unitUnderTest.addBook(renderBook())).extracting(Book::getBookName).isEqualTo("Dziady");
-    }
-    @Test
-    public void deleteBook() {
-        when(bookRepository.deleteByName(any())).thenReturn(renderBook());
-        assertThat(unitUnderTest.deleteBook(renderBook())).extracting(Book::getBookName).isEqualTo("Dziady");
-    }
-    private Book renderBook() {
-        return Book.builder()
-                .id(UUID.randomUUID())
-                .bookName("Dziady")
-                .pageAmount("234")
-                .author("Adam Mickiewicz")
-                .build();
-
-    }
 }
